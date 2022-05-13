dc2b7f1bdc4b0454486de21642657f01e661a058
Naga8 <naga.vj07@gmail.com>
2022-05-13 13:12:43 +0530

    first commit

--- test.js
+++ test.js
@@ -14,28 +14,28 @@
 //     });
 // }
 
-const a = {x:1, y:2, c: {d:4}}
-const b = {...a};
-a.x = 3
-a.y = 4
-a.c.d = 3
-console.log(a)
-console.log(b)
-
-const oldObj = {a: {b: 10}, c: 2};
-const newObj = {...oldObj};
-
-oldObj.a.b = 2; // It also changes the newObj `b` value as `newObj` and `oldObj`'s `b` property allocates the same memory address.
-oldObj.c = 5; // It changes the oldObj `c` but untouched at the newObj
-
-
-
-console.log('oldObj:', oldObj);
-console.log('newObj:', newObj);
-
-// let a =10
-// var a = 20
+// const a = {x:1, y:2, c: {d:4}}
+// const b = {...a};
+// a.x = 3
+// a.y = 4
+// a.c.d = 3
 // console.log(a)
+// console.log(b)
+
+// const oldObj = {a: {b: 10}, c: 2};
+// const newObj = {...oldObj};
+
+// oldObj.a.b = 2; // It also changes the newObj `b` value as `newObj` and `oldObj`'s `b` property allocates the same memory address.
+// oldObj.c = 5; // It changes the oldObj `c` but untouched at the newObj
+
+
+
+// console.log('oldObj:', oldObj);
+// console.log('newObj:', newObj);
+
+let a =10
+var a = 20
+console.log(a)
 
 
 
